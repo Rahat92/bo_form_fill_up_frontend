633a81075c2396c669d03be986fa77e4908c1935
Rahat92 <khrahat92@gmail.com>
2024-11-10 19:01:29 +0600

    add a env file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import MyEditor from './components/MyEditor';
 import DownloadZipFile from './components/DownloadZipFile';
 import '../src/App.css'
 const TextToJsonConverter = () => {
+    console.log(process.env.REACT_APP_IP)
     return (
         <div>
             <MyEditor />
