1ecfe32980cbeffc2f936a2b0ac45f2a4601659c
Rahat92 <khrahat92@gmail.com>
2024-11-11 18:46:57 +0600

    11-11-2024

--- src/components/MyEditor.jsx
+++ src/components/MyEditor.jsx
@@ -7,7 +7,9 @@ const MyEditor = () => {
     const [inputText, setInputText] = useState('');
     const [windowWidth, setWindowWidth] = useState()
     const [isSubmitButtonClicked, setIsSubmitButtonClicked] = useState()
-    const [clientId, setClientId] = useState()
+    const [clientId, setClientId] = useState();
+    const [loading, setLoading] = useState(false);
+    const [serverResponse, setServerResponse] = useState('')
     useEffect(() => {
         setWindowWidth(window.innerWidth)
         window.addEventListener('resize', () => {
@@ -65,14 +67,15 @@ const MyEditor = () => {
 
     const convertToJson = async () => {
         const result = extractDataFromHTML(inputText)
-        console.log('haha',Object.keys(result))
+        console.log('haha', Object.keys(result))
         const date = formatDate(result['জন্ম তারিখ']) || formatDate(result['Date of Birth'])
+        setLoading(true)
         const response = await fetch('http://157.142.6.2:3001/modify-pdf', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(
                 {
-                    clientId:clientId,
+                    clientId: clientId,
                     clientName: result['একক আবেদনকারী নাম'] || result["Single Applicant Name"] || result["1st Applicant Name"],
                     clientGender: result['লিঙ্গ'] || result['Gender'],
                     clientEmail: result['ইমেইল'] || result["Email"],
@@ -102,52 +105,62 @@ const MyEditor = () => {
                 }
             )
         })
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        window.open(url, "_blank");
+        const res = await response.json()
+        setServerResponse(res.message)
+        setLoading(false)
+        // const blob = await response.blob();
+        // const url = window.URL.createObjectURL(blob);
+        // window.open(url, "_blank");
     };
 
     useEffect(() => {
-        if(isSubmitButtonClicked) {
+        if (isSubmitButtonClicked) {
             const clientId = prompt('Add client Id.')
             setClientId(clientId)
         }
         return () => setIsSubmitButtonClicked(false)
     }, [isSubmitButtonClicked])
     useEffect(() => {
-        if(clientId) {
+        if (clientId) {
             convertToJson()
         }
     }, [clientId])
     return (
-        <div className='flex justify-center flex-col items-center mt-8 gap-8'>
-            <div className='flex flex-col items-center gap-2 h-[15vh]'>
-                <img src={logo} width="100px" />
-                <h1 className='text-3xl font-semibold'>BO form fill up</h1>
-            </div>
-            <motion.div className=''>
-                <div className='flex flex-col items-center gap-4 h-[85]'>
-                    <Editor
-                        className=""
-                        apiKey="t07rqm8g7iq1q374jkgsazk2vgbmxdowxpa25njpkiwbwj1b"
-                        init={{
-                            height: windowWidth <= 1600 ? 400 : 600,
-                            width: 500,
-                            menubar: false,
-                            plugins: ['link'],
-                            toolbar: 'undo redo | formatselect | bold italic | link | underline',
-                        }}
-                        onEditorChange={handleEditorChange}
+        <>
+            {loading && (
+                <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex justify-center items-center text-2xl'>Generating Folder, Please wait</div>
+            )}
+            <div className={`${loading?'hidden':'flex'} justify-center flex-col items-center mt-8 gap-8`}>
 
-                    />
-                    <div className='flex justify-center w-full items-center'>
-                        {/* <button type='submit' onClick={convertToJson} className='border rounded-sm p-2'>Fill BO form</button> */}
-                        <button type='submit' onClick={() => setIsSubmitButtonClicked(true)} className='border rounded-sm p-2'>Fill BO form</button>
-                    </div>
+                <div className='flex flex-col items-center gap-2 h-[15vh]'>
+                    <img src={logo} width="100px" />
+                    <h1 className='text-3xl font-semibold'>BO form fill up</h1>
                 </div>
-                {/* <div dangerouslySetInnerHTML={{ __html: content }} /> */}
-            </motion.div>
-        </div>
+                <motion.div className=''>
+                    <div className='flex flex-col items-center gap-4 h-[85]'>
+                        <Editor
+                            className=""
+                            apiKey="t07rqm8g7iq1q374jkgsazk2vgbmxdowxpa25njpkiwbwj1b"
+                            init={{
+                                height: windowWidth <= 1600 ? 400 : 600,
+                                width: 500,
+                                menubar: false,
+                                plugins: ['link'],
+                                toolbar: 'undo redo | formatselect | bold italic | link | underline',
+                            }}
+                            onEditorChange={handleEditorChange}
+                            onFocus={() => setServerResponse('')}
+                        />
+                        <div className='flex flex-col justify-center w-full items-center gap-2'>
+                            <div className='text-red-500'>{serverResponse?<p>{serverResponse}</p>:''}</div>
+                            {/* <button type='submit' onClick={convertToJson} className='border rounded-sm p-2'>Fill BO form</button> */}
+                            <button type='submit' onClick={() => setIsSubmitButtonClicked(true)} className='border rounded-sm p-2'>Fill BO form</button>
+                        </div>
+                    </div>
+                    {/* <div dangerouslySetInnerHTML={{ __html: content }} /> */}
+                </motion.div>
+            </div>
+        </>
     );
 };
 
